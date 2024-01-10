7f9b6787acc2cc1d78b54a3dd80c7148c9320bbd
volodkindv <volodkindv@skbkontur.ru>
2024-01-10 17:22:36 +0500

    readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 MVP сервиса для сбора и обработки подписей Госключом под массовыми обращениями в госорганы.
+
+Пример запуска:
+
+`python main.py --source tests/fixtures/mass --out list.ods`
+
+Параметры:
+
+`python main.py -h`
--- src/cli.py
+++ src/cli.py
@@ -6,7 +6,7 @@ from .main import extract_cert_infos_from_directory, write_signatures_to_file
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--source", type=str, help="source directory with .sig files", required=True)
-    parser.add_argument("-o", "--output", type=str, help="result filename")
+    parser.add_argument("-o", "--output", type=str, help="result filename (ODS format)")
 
     args = parser.parse_args()
     extract(args.source, args.output)
